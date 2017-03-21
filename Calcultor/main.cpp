a540f35b2d240fa7ff6542ab9580be3a215b9a33
czldl <czldl@tom.com>
2017-03-21 20:11:09 +0800

    修复许多Bugs，cin.fail(),cin.clear(),cin.ignore()等使用

--- Calcultor/main.cpp
+++ Calcultor/main.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<cstdlib>
+#include <typeinfo>
+#include<cstring>
+
 using namespace std;
 
 class DivideByZero {};	// 定义一个空类，0除错误
@@ -8,24 +11,32 @@ class OperationTypeNoExist {};// 定义一个空类，操作类型不存在
 class Calculator{
 	double operand1,operand2;
 	char operat;
+	static double result;
 public:
 	Calculator(double op1,char oper,double op2){
 		operand1=op1;operand2=op2;operat=oper;
+		result=operand1;
+	}
+	Calculator(char oper,double op2){
+		operand2=op2;operat=oper;
 	}
 	double Calculate();	
 };
+double Calculator::result=0;
+
 double Calculator::Calculate(){
+	operand1 = result;
 	try{
 		if(operat=='+')
-			return operand1+operand2;
+			return result = operand1+operand2;
 		else if(operat=='-')
-			return operand1-operand2;
+			return result = operand1-operand2;
 		else if(operat=='*')
-			return operand1*operand2;
+			return result = operand1*operand2;
 		else if(operat=='/'){
 			if(operand2==0)
 				throw DivideByZero();
-			return operand1/operand2;
+			return result = operand1/operand2;
 		}
 		else
 			throw OperationTypeNoExist();// 抛掷无操作类型异常	
@@ -40,41 +51,77 @@ double Calculator::Calculate(){
 
 
 int main (int argc, char *argv[]) {
-	/*Calculator ca1(2.5,'+',1);
-	cout<<ca1.Calculate()<<endl;
-	Calculator ca2(2.5,'-',1);
-	cout<<ca2.Calculate()<<endl;
-	Calculator ca3(2.5,'*',1);
-	cout<<ca3.Calculate()<<endl;
-	Calculator ca4(2.5,'/',1);
-	cout<<ca4.Calculate()<<endl;*/
-	double d1,d2;
+	double d1,d2,temp;
 	char op='=';
-	cout<<"请输入被操作数，操作符，操作数"<<endl;
-	cin>>d1>>op>>d2;
-	double temp;
-	while(op!='='){
-		
+	try{
+		cout<<"请输入被操作数，操作符，操作数"<<endl;
+		cin>>d1>>op>>d2;
+		while(cin.fail()){
+			cout<<"输入数据类型错误！\n请重新输入被操作数，操作符，操作数"<<endl;
+			cin.clear();
+			cin.ignore(1024,'\n');
+			cin>>d1>>op>>d2;
+		}
 		Calculator ca1(d1,op,d2);
-		try{
+		temp=ca1.Calculate();
+		cout<<"此次计算结果为: "<<temp<<endl;
+		
+		while(cout<<"请输入下一个操作符: ",cin>>op,op!='='){
+			cout<<"请输入下一个操作数: ";
+			cin>>d2;
+			while(cin.fail()){
+				cout<<"输入数据类型错误！\n请重新输入操作数"<<endl;
+				cin.clear();
+				cin.ignore(1024,'\n');
+				cin>>d2;
+			}
+			Calculator ca1(op,d2);
 			temp=ca1.Calculate();
-			cout<<temp<<endl;
+			cout<<"此次计算结果为: "<<temp<<endl;
 		}
-		catch(DivideByZero){
-			cout<<"除数不能为零！"<<endl;
-			system("pause");
-			exit(0);
-		}
+	}
+	catch(DivideByZero){
+		cout<<"除数不能为零！"<<endl;
+		system("pause");
+		exit(0);
+	}
 		catch(OperationTypeNoExist){
 			cout<<"操作符错误，不能计算！"<<endl;
 			system("pause");
 			exit(0);
 		}
-		cout<<"请输入操作符，操作数"<<endl;
-		cin>>op>>d2;
-		d1=temp;
-	}
+	cout<<"最终计算结果为: "<<temp<<endl;
+	system("pause");
 		
 	return 0;
 }
 
+/*
+请输入被操作数，操作符，操作数
+2*u
+输入数据类型错误！
+请重新输入被操作数，操作符，操作数
+2*3
+此次计算结果为: 6
+请输入下一个操作符: *
+请输入下一个操作数: 4
+此次计算结果为: 24
+请输入下一个操作符: *
+请输入下一个操作数: e
+输入数据类型错误！
+请重新输入操作数
+2
+此次计算结果为: 48
+请输入下一个操作符: /
+请输入下一个操作数: 6
+此次计算结果为: 8
+请输入下一个操作符: -
+请输入下一个操作数: @
+输入数据类型错误！
+请重新输入操作数
+1
+此次计算结果为: 7
+请输入下一个操作符: =
+最终计算结果为: 7
+请按任意键继续. . .
+*/
