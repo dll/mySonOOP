330371a86e60653e78da05217580daae450a6f11
czldl <czldl@tom.com>
2017-03-26 20:08:34 +0800

    6种循环结构！

--- Goldbach/main.cpp
+++ Goldbach/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	Goldbach g1(6,20000);
+	Goldbach g1(6,20000000);
 	g1.GoldbachValid();
 	g1.GoldbachEnum();
 	return 0;
---
+++ looper/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int a;
+	for(a=1;a<=10;a++){//7,8行代码，至少循环0次
+		cout<<"for:"<<a<<" ";
+		if(a==5)
+		goto DOWHILE;
+	}
+	
+	a=1;
+	while(a<=10){//10,11,12,13,14行代码，至少循环0次
+		DOWHILE:
+		cout<<"while:"<<a<<" ";
+		//continue;//继续
+		//break;//中断
+		a=a+1;
+		
+	}
+	
+	a=1;
+	do{
+		cout<<"dowhile:"<<a<<" ";
+		a=a+1;
+	} while(a<=10);//16,17,18,19,20行代码，至少循环1次
+	
+	system("pause");
+	return 0;
+}
+/*
+for:最简洁
+while:功能与for等价
+dowhile:至少执行一次循环体
+continue:跳过下面语句，继续循环
+break:中断循环
+goto:可以跳到标志的位置
+*/
